c415a9ede57cb9067864a6f6617bbe70bd44392f
bbloch <b9bloch@yahoo.fr>
2021-06-18 17:02:48 +0200

    Git init

---
+++ RxMVVM/Sources/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  RxMVVM
+//
+//  Created by Bertrand BLOC'H on 18/06/2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
